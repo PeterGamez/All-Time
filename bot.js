a3dec911717d8b5c2d39c2f2bb55a88e39fdfe71
PeterGamez <64685452+PeterGamez@users.noreply.github.com>
2021-08-26 10:58:37 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js"); require('discord-reply');
+const client = new Discord.Client({ disableMentions: 'everyone' }); require('discord-buttons')(client);
+client.config = require('../config.json');
+client.login(client.config.token.main);
+
+["aliases", "commands", "cooldowns"].forEach(x => client[x] = new Discord.Collection());
+["command", "console", "event"].forEach(x => require(`./handlers/${x}`)(client));
